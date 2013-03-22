68e7047a39a944814cc094130b66f290461212f0
Chris Kohn <ckohn@xilinx.com>
2013-03-22 15:31:28 -0700

    initial commit - ADI originals

---
+++ SDK/SDK_Workspace/bin/download.tcl
@@ -0,0 +1,23 @@
+
+set currentWorkingDirectory [pwd]
+
+set bitFileName "/system.bit"
+set tclFileName "/ps7_init.tcl"
+set elfFileName "/sw.elf"
+
+set elfFilePath $currentWorkingDirectory$elfFileName
+set idx [string last "bin" $currentWorkingDirectory]
+set currentWorkingDirectory [string replace $currentWorkingDirectory $idx [string length $currentWorkingDirectory] "hw"]
+set bitFilePath $currentWorkingDirectory$bitFileName
+set tclFilePath $currentWorkingDirectory$tclFileName
+
+fpga -debugdevice devicenr 2 -f $bitFilePath
+connect arm hw
+rst -processor
+source $tclFilePath
+ps7_init
+init_user
+dow $elfFilePath
+run
+disconnect 64
+exit
